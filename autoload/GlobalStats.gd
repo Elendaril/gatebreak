5b0d4c9893bc0c2bedbfa8b3c6f42034b66ccef1
Paweł Kuta <elendaril@gmail.com>
2025-06-05 14:15:01 +0200

    Changes in GlobalStats, added calculatic hit chance and overhit

--- autoload/GlobalStats.gd
+++ autoload/GlobalStats.gd
@@ -1,1 +1,68 @@
+# "res://autoload/GlobalStats.gd"
 extends Node
+
+# Max HP calculation
+func calculate_max_hp(base_hp: int, vit: int, str_val: int) -> int:
+	return base_hp + roundi(3 * vit) + str_val
+
+# Max MP calculation	
+func calculate_max_mp(base_mp: int, spirit: int) -> int:
+	return base_mp + roundi(3.0 * spirit)
+
+# MP regen calculation - per turn 
+func calculate_mp_regen(base_mp_regen: int, spirit: int) -> int:
+	return base_mp_regen + roundi(0.5 * spirit)
+	
+# Unit physical damage
+func calculate_physical_damage(base_dmg: int, str_val: int) -> int:
+	return base_dmg + roundi(1.5 * str_val)
+
+# Unit magic damage
+func calculate_magic_damage(base_dmg: int, intel: int) -> int:
+	return base_dmg + roundi(2.0 * intel)
+
+# Unit defense without equipment
+func calculate_defense(base_def: int, vit: int) -> int:
+	return base_def + roundi(0.8 * vit)
+
+# Unit magic defense without equipment
+func calculate_magic_defense(base_mdef: int, intel: int) -> int:
+	return base_mdef + roundi(0.7 * intel)
+
+# Unit dodge rating without equipment
+func calculate_dodge_rating(base_dodge: int, spd: int, lck: int) -> int:
+	return base_dodge + roundi((2 * spd) + (0.5 * lck))
+
+# Unit hit rate without equipment 
+# If unit hit rate >= enemy dodge rateing hit chance == 100%
+# if unit hit rate < enemy dodge rateing. (unit_hit_rate / enemy_dodge_rating) * 100 
+func calculate_hit_rating(base_hit: int, acc:int, lck: int) ->int:
+	return base_hit + roundi((2 * acc) + (0.5 * lck))
+
+# Unit crit chance without equipment
+func calculate_crit_chance(base_crit: int, acc: int, lck: int) -> int:
+	return base_crit + roundi((0.1 * acc) + (0.3 * lck))
+
+func calculate_hit_chance(unit_hit_rate: int,enemy_dodge_rate:int) -> int:
+	if enemy_dodge_rate <= 0:
+		return 999
+	var hit_chance_ratio = float(unit_hit_rate) / enemy_dodge_rate
+	var hit_chance_percent = hit_chance_ratio * 100.0
+	return roundi(hit_chance_percent)
+		
+func overhit_to_crit_chance(hit_chance:int) -> int:
+	if hit_chance > 100:
+		return hit_chance - 100
+	else:
+		return 0
+
+# Weapons stat scaling for damage
+const SCALING_RANK_MULTIPLIERS = {
+	"S": 2.0,
+	"A": 1.6,
+	"B": 1.25,
+	"C": 1.0,
+	"D": 0.75,
+	"E": 0.5,
+	"F": 0.25
+}
